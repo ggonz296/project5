93c1f2e57e3293e4d500784db909ed3e5dfae829
Gabriel Gonzalez <ggonz296@stu.southtexascollege.edu>
2025-02-25 16:19:46 -0600

    Initialized Card Components

---
+++ src/components/MainCardSingle.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { faker } from '@faker-js/faker'
+//change these
+const firstname = faker.person.firstName()
+const lastname = faker.person.lastName()
+
+</script>
+
+<template>
+    {{ firstname }} {{ lastname }} <br/>
+</template>
---
+++ src/components/MainCards.vue
@@ -0,0 +1,8 @@
+<script setup>
+    import MainCardSingle from './MainCardSingle.vue'
+</script>
+
+<template>
+    <!--Change the number of cards-->
+    <MainCardSingle v-for="card in 10" :key="card"></MainCardSingle>
+</template>
